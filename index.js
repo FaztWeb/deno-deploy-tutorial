fd67251cc97d0743ee7942c8ab24258f0fef0d84
fazt <fazt@faztweb.com>
2022-07-17 15:50:48 -0500

    oak routes

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak@v10.6.0/mod.ts";
 
 const app = new Application();
+const tasks = [];
 
-app.use((ctx) => {
-  ctx.response.body = "Hello World with Oak!";
-});
+const tasksRoutes = new Router()
+  .get("/", (ctx) => {
+    ctx.response.body = "Welcome to my API";
+  })
+  .get("/tasks", (ctx) => {
+    ctx.response.body = tasks;
+  })
+  .post("/tasks", async (ctx) => {
+    const newTask = await ctx.request.body().value;
+    tasks.push(newTask);
+    ctx.response.body = newTask;
+  });
 
+app.use(tasksRoutes.routes());
+
+console.log("Server running on port 8000");
 await app.listen({ port: 8000 });
